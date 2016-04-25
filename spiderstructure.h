a3173c150da04f10e68950a2119b01115f3213b2
nathanyoung1 <nathan.young.wsu@gmail.com>
2016-04-25 11:15:09 -0700

    Update spiderstructure.h

--- spiderstructure.h
+++ spiderstructure.h
@@ -6,15 +6,22 @@
 class spider : public sf::RectangleShape
 {
 public:
-	spider(float positionx = 0, float positiony = 0);
+	//spider(float positionx, float positiony);
+	spider();
 	~spider();
 	sf::RectangleShape getleg(int which);
 	sf::CircleShape getbody();
 	void move(float offsetX, float offsetY);
-
+	void setposition(float positionX, float positiony);
+	sf::Vector2i getPosition();
+	bool iskilled();
+	bool isactive();
+	void kill();
+	void activate();
 private:
 	sf::RectangleShape leg[16];
 	sf::CircleShape body;
-	float positionx;
-	float positiony;
+	sf::Vector2i position;
+	bool killed;
+	bool active;
 };
