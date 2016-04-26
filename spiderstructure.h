fb50019533539919ab298915b960675ff65cfe6a
nathanyoung1 <nathan.young.wsu@gmail.com>
2016-04-26 11:16:10 -0700

    Update spiderstructure.h

--- spiderstructure.h
+++ spiderstructure.h
@@ -1,27 +1,26 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+using std::cout;
 
 
-class spider : public sf::RectangleShape
+class spider
 {
 public:
-	//spider(float positionx, float positiony);
+	
 	spider();
 	~spider();
-	sf::RectangleShape getleg(int which);
-	sf::CircleShape getbody();
+	
 	void move(float offsetX, float offsetY);
 	void setposition(float positionX, float positiony);
-	sf::Vector2i getPosition();
-	bool iskilled();
-	bool isactive();
-	void kill();
-	void activate();
+	sf::Vector2f getPosition();
+	spider* getnext();
+	void setnext(spider *next);
+	spider* getprev();
+	void draw(sf::RenderWindow &window);
 private:
-	sf::RectangleShape leg[16];
-	sf::CircleShape body;
-	sf::Vector2i position;
-	bool killed;
-	bool active;
+	sf::Sprite it;
+	sf::Texture tit;
+	spider *pNext;
+	spider *pLast;
 };
