44c5f7813e94e9edc9ef9cf52aabeae2868e8a2a
nathanyoung1 <nathan.young.wsu@gmail.com>
2016-04-30 16:40:55 -0700

    Delete physics.h

--- physics.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "spiderspawner.h"
-#include "shoe.h"
-bool collision(sf::Vector2i tempSpritePosition, sf::Vector2i tempSpriteWH,
-	sf::Vector2i tempCollisionBoxPosition, sf::Vector2i tempCollisionBoxWH);
-
-bool collisionDetection(spiderspawner &test, Shoe &shoe);
